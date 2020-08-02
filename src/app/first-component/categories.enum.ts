560b0a972c4b22f7f0aef32fbee52ee159816ce3
Timofei Kuznetsov <Timofei_Kuznetsov@epam.com>
2020-08-02 21:30:27 +0300

    Add FirstComponent

---
+++ src/app/first-component/categories.enum.ts
@@ -0,0 +1,4 @@
+export enum Categories {
+    ALL = 1,
+    OTHER
+}
---
+++ src/app/first-component/first-component.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Categories} from './categories.enum';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.scss']
+})
+export class FirstComponentComponent implements OnInit {
+  name: string;
+  description: string;
+  price: number;
+  category: Categories;
+  isAvailable: boolean;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
